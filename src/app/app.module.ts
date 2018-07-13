e41083a0e88900a0df019e779323e53cd1737473
Dasharath Bhukya (db436t) <db436t@att.com>
2018-07-13 18:45:43 +0530

    added home tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicImageViewerModule } from 'ionic-img-viewer';
 
 import { MyApp } from './app.component';
 import { CustomPopup } from '../pages/popup/custom-popup';
@@ -14,6 +15,7 @@ import { CustomPopup } from '../pages/popup/custom-popup';
   ],
   imports: [
     BrowserModule,
+    IonicImageViewerModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
